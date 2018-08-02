a127088cdcb9f51f356e48a54d0a97820d32b558
Facundo Alvarado <facundoalvarado9@gmail.com>
2018-08-02 16:32:19 -0300

    Progress

--- app/src/main/java/com/facundoalvarado/mascotapp/Fragments/VacunasFragment.java
+++ app/src/main/java/com/facundoalvarado/mascotapp/Fragments/VacunasFragment.java
@@ -1,7 +1,5 @@
 package com.facundoalvarado.mascotapp.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -20,6 +18,8 @@ public class VacunasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vacunas, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_vacunas_perros, container, false);
+
+        return rootView;
     }
 }
