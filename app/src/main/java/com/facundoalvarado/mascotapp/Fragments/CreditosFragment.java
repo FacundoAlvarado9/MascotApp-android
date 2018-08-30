c64be090bedb71ac4875c095bd51d42acb537a47
Facundo Alvarado <facundoalvarado9@gmail.com>
2018-08-29 21:58:13 -0300

    Progress

---
+++ app/src/main/java/com/facundoalvarado/mascotapp/Fragments/CreditosFragment.java
@@ -0,0 +1,37 @@
+package com.facundoalvarado.mascotapp.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facundoalvarado.mascotapp.R;
+public class CreditosFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public CreditosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_creditos, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/facundoalvarado/mascotapp/Fragments/VeterinariasFragment.java
+++ app/src/main/java/com/facundoalvarado/mascotapp/Fragments/VeterinariasFragment.java
@@ -1,14 +1,18 @@
 package com.facundoalvarado.mascotapp.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.facundoalvarado.mascotapp.MainActivity;
 import com.facundoalvarado.mascotapp.R;
+import com.facundoalvarado.mascotapp.VeterinariasActivity;
 
 public class VeterinariasFragment extends Fragment {
 
@@ -19,6 +23,21 @@ public class VeterinariasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_veterinarias, container, false);
+        View  rootView = inflater.inflate(R.layout.fragment_veterinarias, container, false);
+
+        Button btnMapa = (Button) rootView.findViewById(R.id.btnMapa);
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), VeterinariasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return rootView;
     }
+
+
 }
